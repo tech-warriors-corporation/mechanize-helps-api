b9c525c11217f5a1e7eeaf7461b1335fe5d6b1e2
hotequil <hotequil@protonmail.com>
2023-03-13 07:32:13 -0300

    feat: removed test code

--- app.py
+++ app.py
@@ -18,23 +18,5 @@ load_dotenv(find_dotenv())
 
 TicketsController(app, TicketsService(TicketRepository()))
 
-users_client = UsersClient('http://localhost:8000/api/accounts/users')
-driver_id = users_client.create('João (driver)', f'joao-{uuid4()}@driver.com', 'JoaoDriver1234', UserRoleEnum.DRIVER.value)['payload']
-mechanic_id = users_client.create('João (mechanic)', f'joao-{uuid4()}@mechanic.com', 'JoaoMechanic1234', UserRoleEnum.MECHANIC.value)['payload']
-driver = users_client.get(driver_id)['payload']
-mechanic = users_client.get(mechanic_id)['payload']
-client = app.test_client()
-
-ticket_id = client.post(f"http://localhost:{port}/api/helps/tickets", json={
-    'driver_id': driver['id'],
-    'mechanic_id': mechanic['id'],
-    'vehicle': 'Mercedes-Benz CLS53 AMG 3.0',
-    'location': 'São Paulo, Brazil',
-    'description': f'Envolved users emails are: {driver["email"]} and {mechanic["email"]}',
-    'status': TicketStatusEnum.ATTENDING.value,
-}).json['payload']
-
-print(f"Created ticket with id: {ticket_id}")
-
 if __name__ == '__main__':
     app.run(debug=True, port=port)
