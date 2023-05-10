cdc79c2ca50be8040836a794838b252175a8df90
hotequil <hotequil@protonmail.com>
2023-05-09 21:44:04 -0300

    feat: removed some methods in users_client and verified if request is from correct client_id

--- controllers/tickets_controller.py
+++ controllers/tickets_controller.py
@@ -2,7 +2,7 @@ from services.tickets_service import TicketsService
 from response import generate_response
 from controllers.controller import Controller
 from flask import Flask, request
-from request import should_be_logged
+from request import should_be_logged, should_be_valid_client_id
 
 class TicketsController(Controller):
     def __init__(self, app: Flask, tickets_service: TicketsService):
@@ -13,6 +13,7 @@ class TicketsController(Controller):
     def register_routes(self):
         self._app.add_url_rule('/api/helps/tickets', 'create', self.create, methods=['POST'])
 
+    @should_be_valid_client_id
     @should_be_logged
     def create(self):
         try:
--- request.py
+++ request.py
@@ -2,19 +2,35 @@ from clients.users_client import UsersClient
 from os import environ
 from flask import request
 from response import generate_response
+from os import environ
 
 users_client = UsersClient(environ.get("ACCOUNTS_API_URL"))
+valid_client_id = environ.get('CLIENT_ID')
 
 def should_be_logged(callback):
     def secure_function(*args, **kwargs):
         token = request.headers.get('Authorization')
+        client_id = request.headers.get('clientId')
 
         if token is None:
             return generate_response(status_code=401)
 
-        is_valid_token: bool = users_client.has_valid_token(token)['payload']
+        is_valid_token: bool = users_client.has_valid_token(token, client_id)['payload']
 
         if is_valid_token is True:
+            return callback(*args, **kwargs)
+        else:
+            return generate_response(status_code=401)
+
+    secure_function.__name__ = callback.__name__
+
+    return secure_function
+
+def should_be_valid_client_id(callback):
+    def secure_function(*args, **kwargs):
+        client_id = request.headers.get('clientId')
+
+        if client_id == valid_client_id:
             return callback(*args, **kwargs)
         else:
             return generate_response(status_code=401)
